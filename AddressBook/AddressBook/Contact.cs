18114108fc0286c3148d1976afcb7705089e6b47
Daneesh <mustafadaneesh@gmail.com>
2020-10-04 22:53:21 +0530

    [Daneesh] Refactor : Adding new contact using console

--- AddressBook/AddressBook/Contact.cs
+++ AddressBook/AddressBook/Contact.cs
@@ -8,11 +8,11 @@ namespace AddressBook
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string address { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public int zip { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
         public long PhoneNumber { get; set; }
-        public string email { get; set; }
+        public string Email { get; set; }
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,8 +5,36 @@ namespace AddressBook
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             Console.WriteLine("Welcome To Address Book Program on Master Branch ");
+            Console.WriteLine("\n");
+            Console.WriteLine("Add the Contact Details ");
+            Console.WriteLine("\n");
+            AddressBookSystem addressbook = new AddressBookSystem();           
+            Contact contact = new Contact();
+            SetDetails(contact);
+            addressbook.AddContact(contact); 
+        }
+        public static void SetDetails(Contact contact)
+        {
+            Console.WriteLine("First Name : ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Last Name :");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Address :");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("City :");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("State :");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Zip Code :");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Phone Number :");
+            contact.PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Email Address :");
+            contact.Email = Console.ReadLine();
         }
     }
+    
 }
+
