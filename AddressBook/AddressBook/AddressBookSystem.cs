f52290d3b2a50365ee786f12dfafccbddde3be9d
Daneesh <mustafadaneesh@gmail.com>
2020-10-04 18:17:00 +0530

    [Daneesh] Refactor : Created AddressBookSystem Class

---
+++ AddressBook/AddressBook/AddressBookSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBookSystem
+    {
+        public List<Contact> ContactList;
+        public AddressBookSystem()
+        {
+            this.ContactList = new List<Contact>();
+        }
+    }
+}
